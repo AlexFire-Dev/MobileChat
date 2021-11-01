338e75a390e1cf35f3232d97fc04db15f19be3a0
Alex Shvarev 2 <alex@shvarev.com>
2021-11-01 17:25:59 +0300

    v0.2 added chat admin

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Guild)
+class GuildAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created')
+    list_display_links = ('name',)
+
+
+@admin.register(models.Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+
+
+@admin.register(models.Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'guild', 'admin', 'active', 'banned')
+
+
+@admin.register(models.Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author')
+
+
+@admin.register(models.Invitation)
+class InvitationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'guild', 'key')
---
+++ apps/chat/serializers.py
@@ -0,0 +1,7 @@
+import json
+
+import channels.layers
+from asgiref.sync import async_to_sync
+from rest_framework import serializers
+
+from . import models
